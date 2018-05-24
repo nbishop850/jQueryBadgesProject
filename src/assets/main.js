2ee22d73726e9140a47101f2eed5b4bfae635958
Nate Bishop <nbishop850@gmail.com>
2018-05-24 16:06:03 -0700

    Initial Build, AJAX course Divs

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,5 +1,28 @@
+var badgeURL = 'https://www.codeschool.com/users/noise850.json';
+var account = new Object();
 $(function() {
 
-  // your code will go here
-
+    $.ajax({
+        url: badgeURL,
+        dataType: 'jsonp',
+        success: function(response){
+            handleJSON(response);
+        }
+    });
 });
+
+function handleJSON(json){
+    for(var key in json){
+        account[key] = json[key];
+    }
+    console.log(account);
+    var badgesDiv = $('#badges');
+    for(var i in account.courses.completed){
+        var course = account.courses.completed[i];
+        console.log(course.title);
+        var courseDiv = $('<div>');
+        courseDiv.addClass("course").html("<h3>" + course.title + "</h3>");
+        courseDiv.append($('<img>', {src: course.badge}));
+        badgesDiv.append(courseDiv);
+    }
+};
